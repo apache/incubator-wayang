2eb942dc6d689b95253fabd6947aa2274435193c
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-01-11 13:41:28 +0300

    feat(rheem-java): amend missing mapping for ReduceBy operator

---
+++ rheem-platforms/rheem-java/src/main/java/org/qcri/rheem/java/mapping/ReduceByOperatorToJavaReduceByOperatorMapping.java
@@ -0,0 +1,41 @@
+package org.qcri.rheem.java.mapping;
+
+import org.qcri.rheem.basic.operators.MapOperator;
+import org.qcri.rheem.basic.operators.ReduceByOperator;
+import org.qcri.rheem.basic.operators.TextFileSource;
+import org.qcri.rheem.core.mapping.*;
+import org.qcri.rheem.core.plan.Operator;
+import org.qcri.rheem.java.operators.JavaMapOperator;
+import org.qcri.rheem.java.operators.JavaReduceByOperator;
+import org.qcri.rheem.java.operators.JavaTextFileSource;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Mapping from {@link TextFileSource} to {@link JavaTextFileSource}.
+ */
+public class ReduceByOperatorToJavaReduceByOperatorMapping implements Mapping {
+
+    @Override
+    public Collection<PlanTransformation> getTransformations() {
+        return Collections.singleton(new PlanTransformation(createSubplanPattern(), new ReplacementFactory()));
+    }
+
+    private SubplanPattern createSubplanPattern() {
+        final OperatorPattern operatorPattern = new OperatorPattern(
+                "reduceBy", new ReduceByOperator<>(null, null, null), false);
+        return SubplanPattern.createSingleton(operatorPattern);
+    }
+
+    private static class ReplacementFactory extends ReplacementSubplanFactory {
+
+        @Override
+        protected Operator translate(SubplanMatch subplanMatch) {
+            final ReduceByOperator<?, ?> originalOperator = (ReduceByOperator<?, ?>) subplanMatch.getMatch("reduceBy").getOperator();
+            return new JavaReduceByOperator<>(originalOperator.getType(),
+                    originalOperator.getKeyDescriptor(),
+                    originalOperator.getReduceDescriptor());
+        }
+    }
+}
