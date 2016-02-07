5595a5f320c744444bfa7bc74d03f96416c93ec4
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-02-07 10:18:14 +0300

    refactor(rheem-core): rename ConfigurationProvider to KeyValueProvider

--- rheem-core/src/main/java/org/qcri/rheem/core/api/configuration/MapBasedKeyValueProvider.java
+++ rheem-core/src/main/java/org/qcri/rheem/core/api/configuration/MapBasedKeyValueProvider.java
@@ -6,18 +6,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Implementation of {@link ConfigurationProvider} that uses a {@link Map} to provide a value.
+ * Implementation of {@link KeyValueProvider} that uses a {@link Map} to provide a value.
  */
-public class MapBasedConfigurationProvider<Key, Value> extends ConfigurationProvider<Key, Value> {
+public class MapBasedKeyValueProvider<Key, Value> extends KeyValueProvider<Key, Value> {
 
     private final Map<Key, Value> storedValues = new HashMap<>();
 
-    public MapBasedConfigurationProvider(ConfigurationProvider<Key, Value> parent) {
+    public MapBasedKeyValueProvider(KeyValueProvider<Key, Value> parent) {
         super(parent);
     }
 
     @Override
-    public Value tryToProvide(Key key, ConfigurationProvider<Key, Value> requestee) {
+    public Value tryToProvide(Key key, KeyValueProvider<Key, Value> requestee) {
         Validate.notNull(key);
         return this.storedValues.get(key);
     }
