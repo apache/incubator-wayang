2698a5a324b976bd89a7b7243d612f548c060e4a
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-02-10 14:40:58 +0300

    fix(rheem-core): correct output of SparkReduceByOperator to conform with Rheem

--- rheem-platforms/rheem-spark/src/main/java/org/qcri/rheem/spark/operators/SparkReduceByOperator.java
+++ rheem-platforms/rheem-spark/src/main/java/org/qcri/rheem/spark/operators/SparkReduceByOperator.java
@@ -3,8 +3,10 @@ package org.qcri.rheem.spark.operators;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaRDDLike;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
+import org.qcri.rheem.basic.data.Tuple2;
 import org.qcri.rheem.basic.operators.ReduceByOperator;
 import org.qcri.rheem.core.function.ReduceDescriptor;
 import org.qcri.rheem.core.function.TransformationDescriptor;
@@ -23,12 +25,12 @@ public class SparkReduceByOperator<Type, KeyType>
     /**
      * Creates a new instance.
      *
-     * @param type        type of the reduce elements (i.e., type of {@link #getInput()} and {@link #getOutput()})
+     * @param type             type of the reduce elements (i.e., type of {@link #getInput()} and {@link #getOutput()})
      * @param keyDescriptor    describes how to extract the key from data units
      * @param reduceDescriptor describes the reduction to be performed on the elements
      */
     public SparkReduceByOperator(DataSetType<Type> type, TransformationDescriptor<Type, KeyType> keyDescriptor,
-                                ReduceDescriptor<Type> reduceDescriptor) {
+                                 ReduceDescriptor<Type> reduceDescriptor) {
         super(type, keyDescriptor, reduceDescriptor);
     }
 
@@ -41,14 +43,28 @@ public class SparkReduceByOperator<Type, KeyType>
         final JavaRDD<Type> inputStream = (JavaRDD<Type>) inputRdds[0];
         final PairFunction<Type, KeyType, Type> keyExtractor = compiler.compileToKeyExtractor(this.keyDescriptor);
         Function2<Type, Type, Type> reduceFunc = compiler.compile(this.reduceDescriptor);
-        JavaPairRDD<KeyType, Type> pairStream = inputStream.mapToPair(keyExtractor);
-        final JavaPairRDD<KeyType, Type> outputStream = pairStream.reduceByKey(reduceFunc);
+        final JavaRDD<Type> outputRdd = inputStream.mapToPair(keyExtractor)
+                .reduceByKey(reduceFunc)
+                .map(new TupleConverter<>());
 
-        return new JavaRDDLike[]{outputStream};
+        return new JavaRDDLike[]{outputRdd};
     }
 
     @Override
     public ExecutionOperator copy() {
         return new SparkReduceByOperator<>(getType(), getKeyDescriptor(), getReduceDescriptor());
+    }
+
+    /**
+     * Extracts the value from a {@link scala.Tuple2}.
+     * <p><i>TODO: See, if we can somehow dodge all this conversion, which is likely to happen a lot.</i></p>
+     */
+    private static class TupleConverter<InputType, KeyType>
+            implements Function<scala.Tuple2<KeyType, InputType>, InputType> {
+
+        @Override
+        public InputType call(scala.Tuple2<KeyType, InputType> scalaTuple) throws Exception {
+            return scalaTuple._2;
+        }
     }
 }
