c1364e2a2d76e1b21cc9c9caf63667759bef2751
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-07-10 13:13:12 +0200

    feat(wordcount): let user specify words per line
    * this greatly improves estimation accurracy

--- src/main/scala/org/qcri/rheem/apps/wordcount/WordCountScala.scala
+++ src/main/scala/org/qcri/rheem/apps/wordcount/WordCountScala.scala
@@ -16,16 +16,17 @@ class WordCountScala(platforms: Platform*) {
   /**
     * Run the word count over a given file.
     *
-    * @param inputUrl URL to the file
+    * @param inputUrl     URL to the file
+    * @param wordsPerLine optional estimate of how many words there are in each line
     * @return the counted words
     */
-  def apply(inputUrl: String) = {
+  def apply(inputUrl: String, wordsPerLine: ProbabilisticDoubleInterval = new ProbabilisticDoubleInterval(100, 10000, .8d)) = {
     val rheemCtx = new RheemContext
     platforms.foreach(rheemCtx.register)
 
     rheemCtx
       .readTextFile(inputUrl).withName("Load file")
-      .flatMap(_.split("\\W+"), selectivity = new ProbabilisticDoubleInterval(100, 10000, .8d)).withName("Split words")
+      .flatMap(_.split("\\W+"), selectivity = wordsPerLine).withName("Split words")
       .filter(_.nonEmpty).withName("Filter empty words")
       .map(word => (word.toLowerCase, 1)).withName("To lower case, add counter")
       .reduceByKey(_._1, (c1, c2) => (c1._1, c1._2 + c2._2)).withName("Add counters")
@@ -42,20 +43,31 @@ class WordCountScala(platforms: Platform*) {
 object WordCountScala {
 
   def main(args: Array[String]) {
+    // Parse args.
     if (args.isEmpty) {
-      println("Usage: <main class> <platform(,platform)*> <input file>")
+      println("Usage: <main class> <platform(,platform)*> <input file> [<words per line a..b>]")
       sys.exit(1)
     }
-
     val platforms = Parameters.loadPlatforms(args(0), () => new Configuration)
-
     val inputFile = args(1)
+    val wordsPerLine = if (args.length >= 3) {
+      val Array(low, high) = args(2).split("""\.\.""").map(_.toDouble)
+      new ProbabilisticDoubleInterval(low, high, 0.8)
+    } else null
 
+    // Run wordCount.
     val wordCount = new WordCountScala(platforms: _*)
-    val words = wordCount(inputFile).toSeq.sortBy(-_._2)
+    val words =
+      (if (wordsPerLine != null) {
+        wordCount(inputFile, wordsPerLine)
+      } else {
+        wordCount(inputFile)
+      }).toSeq.sortBy(-_._2)
 
+    // Print results.
     println(s"Found ${words.size} words:")
-    words.foreach(wc => println(s"${wc._2}x ${wc._1}"))
+    words.take(10).foreach(wc => println(s"${wc._2}x ${wc._1}"))
+    if (words.size > 10) print(s"${words.size - 10} more...")
   }
 
 }
