6990faf48dc998cc910f1e26c61df8ab4527f359
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-07-27 16:56:51 +0200

    test(rheem-tests): test integration of Sqlite3Platform

--- rheem-tests/src/test/scala/org/qcri/rheem/tests/ScalaTest.scala
+++ rheem-tests/src/test/scala/org/qcri/rheem/tests/ScalaTest.scala
@@ -7,7 +7,6 @@ import org.qcri.rheem.core.optimizer.cardinality.{CardinalityEstimator, FixedSiz
 import org.qcri.rheem.core.plan.rheemplan.OutputSlot
 import org.qcri.rheem.graphchi.GraphChiPlatform
 import org.qcri.rheem.java.JavaPlatform
-import org.qcri.rheem.postgres.PostgresPlatform
 import org.qcri.rheem.spark.platform.SparkPlatform
 
 /**
@@ -34,7 +33,6 @@ class ScalaTest {
     rheem.register(JavaPlatform.getInstance)
     rheem.register(SparkPlatform.getInstance)
     rheem.register(GraphChiPlatform.getInstance)
-    rheem.register(PostgresPlatform.getInstance)
 
     // Wikipedia abstract of Rheem, California ;)
     val text = Seq(
