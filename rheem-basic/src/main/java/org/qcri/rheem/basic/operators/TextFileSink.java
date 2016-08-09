22f787fa9a0c62c3c754d3d2b549e56b022549db
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-08-09 20:03:14 +0200

    feat(rheem-basic): add TextFileSink
    * should allow to write data quanta to an output file
    * optionally accepts a formatting function

---
+++ rheem-basic/src/main/java/org/qcri/rheem/basic/operators/TextFileSink.java
@@ -0,0 +1,78 @@
+package org.qcri.rheem.basic.operators;
+
+import org.qcri.rheem.core.function.TransformationDescriptor;
+import org.qcri.rheem.core.optimizer.costs.DefaultLoadEstimator;
+import org.qcri.rheem.core.plan.rheemplan.UnarySink;
+import org.qcri.rheem.core.types.DataSetType;
+
+import java.util.Objects;
+
+/**
+ * This {@link UnarySink} writes all incoming data quanta to a text file.
+ */
+public class TextFileSink<T> extends UnarySink<T> {
+
+    protected final String textFileUrl;
+
+    protected final TransformationDescriptor formattingDescriptor;
+
+    /**
+     * Creates a new instance with default formatting.
+     *
+     * @param textFileUrl URL to file that should be written
+     * @param typeClass   {@link Class} of incoming data quanta
+     */
+    public TextFileSink(String textFileUrl, Class<T> typeClass) {
+        this(
+                textFileUrl,
+                new TransformationDescriptor<>(
+                        Objects::toString,
+                        typeClass,
+                        String.class,
+                        new DefaultLoadEstimator(1, 1, .99d, (in, out) -> 10 * in[0]),
+                        new DefaultLoadEstimator(1, 1, .99d, (in, out) -> 1000)
+                )
+        );
+    }
+
+
+    /**
+     * Creates a new instance.
+     *
+     * @param textFileUrl        URL to file that should be written
+     * @param formattingFunction formats incoming data quanta to a {@link String} representation
+     * @param typeClass          {@link Class} of incoming data quanta
+     */
+    public TextFileSink(String textFileUrl,
+                        TransformationDescriptor.SerializableFunction<T, String> formattingFunction,
+                        Class<T> typeClass) {
+        this(
+                textFileUrl,
+                new TransformationDescriptor<>(formattingFunction, typeClass, String.class)
+        );
+    }
+
+    /**
+     * Creates a new instance.
+     *
+     * @param textFileUrl          URL to file that should be written
+     * @param formattingDescriptor formats incoming data quanta to a {@link String} representation
+     */
+    public TextFileSink(String textFileUrl, TransformationDescriptor<T, String> formattingDescriptor) {
+        super(DataSetType.createDefault(formattingDescriptor.getInputType()));
+        this.textFileUrl = textFileUrl;
+        this.formattingDescriptor = formattingDescriptor;
+    }
+
+    /**
+     * Creates a copied instance.
+     *
+     * @param that should be copied
+     */
+    public TextFileSink(TextFileSink<T> that) {
+        super(that);
+        this.textFileUrl = that.textFileUrl;
+        this.formattingDescriptor = that.formattingDescriptor;
+    }
+
+}
