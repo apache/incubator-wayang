b754e877fa4b7193a05103833e6c26be13c54a70
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-09-27 19:44:53 +0200

    fix(rheem-java,rheem-spark): add missing files for the MapPartitionsOperator
    * provide missing Spark mapping
    * provide missing Java mapping and implementation

---
+++ rheem-platforms/rheem-java/src/main/java/org/qcri/rheem/java/mapping/MapPartitionsMapping.java
@@ -0,0 +1,39 @@
+package org.qcri.rheem.java.mapping;
+
+import org.qcri.rheem.basic.operators.MapPartitionsOperator;
+import org.qcri.rheem.core.mapping.*;
+import org.qcri.rheem.core.types.DataSetType;
+import org.qcri.rheem.java.operators.JavaMapPartitionsOperator;
+import org.qcri.rheem.java.platform.JavaPlatform;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Mapping from {@link MapPartitionsOperator} to {@link JavaMapPartitionsOperator}.
+ */
+@SuppressWarnings("unchecked")
+public class MapPartitionsMapping implements Mapping {
+
+    @Override
+    public Collection<PlanTransformation> getTransformations() {
+        return Collections.singleton(new PlanTransformation(
+                this.createSubplanPattern(),
+                this.createReplacementSubplanFactory(),
+                JavaPlatform.getInstance()
+        ));
+    }
+
+    private SubplanPattern createSubplanPattern() {
+        final OperatorPattern operatorPattern = new OperatorPattern(
+                "mappartitions", new MapPartitionsOperator<>(null, DataSetType.none(), DataSetType.none()), false);
+        return SubplanPattern.createSingleton(operatorPattern);
+    }
+
+
+    private ReplacementSubplanFactory createReplacementSubplanFactory() {
+        return new ReplacementSubplanFactory.OfSingleOperators<MapPartitionsOperator>(
+                (matchedOperator, epoch) -> new JavaMapPartitionsOperator<>(matchedOperator).at(epoch)
+        );
+    }
+}
