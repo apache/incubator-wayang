d7f56f1e9e3b31953d989b9cd9de7a04a4e659d5
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-10-20 13:44:47 +0200

    fix(rheem-java,rheem-spark): exclude TSV-to-*** conversions
    * these ChannelConversions need to be adapted to incorporate the schema

--- rheem-platforms/rheem-java/src/main/java/org/qcri/rheem/java/channels/ChannelConversions.java
+++ rheem-platforms/rheem-java/src/main/java/org/qcri/rheem/java/channels/ChannelConversions.java
@@ -65,7 +65,7 @@ public class ChannelConversions {
             STREAM_TO_HDFS_OBJECT_FILE,
             COLLECTION_TO_HDFS_OBJECT_FILE,
             HDFS_OBJECT_FILE_TO_STREAM,
-            HDFS_TSV_TO_STREAM,
+//            HDFS_TSV_TO_STREAM,
             STREAM_TO_HDFS_TSV,
             COLLECTION_TO_HDFS_TSV
     );
