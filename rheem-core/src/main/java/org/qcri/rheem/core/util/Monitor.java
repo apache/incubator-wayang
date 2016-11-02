689ee4ead0822dad4f73c729a67086ecbaa0079d
Yasser Idris <yidris@qf.org.qa>
2016-11-02 11:08:05 +0300

    fixes for Repeat and TableSource operators, update Monitor code, merge with master

--- rheem-core/src/main/java/org/qcri/rheem/core/util/Monitor.java
+++ rheem-core/src/main/java/org/qcri/rheem/core/util/Monitor.java
@@ -15,21 +15,23 @@ import java.util.Map;
 
 // TODO: Rerwrite properly to be more generic, and register properly with application.
 public class Monitor {
-    private Map Progress;
-    private List initialExecutionPlan;
+    private HashMap<String, Integer> progress = new HashMap<>();
+    private List<Map> initialExecutionPlan;
+    private String exPlanUrl;
+    private String progressUrl;
     private String runId;
 
     public static final String DEFAULT_RUNS_DIR = "file:///var/tmp/rheem/runs";
     public static final String DEFAULT_RUNS_DIR_PROPERTY_KEY = "rheem.basic.runsdir";
 
-    public void initialize(Configuration config, String runId, List initialExecutionPlan) throws IOException {
+    public void initialize(Configuration config, String runId, List<Map> initialExecutionPlan) throws IOException {
         this.initialExecutionPlan = initialExecutionPlan;
         this.runId = runId;
         String runsDir = config.getStringProperty(DEFAULT_RUNS_DIR_PROPERTY_KEY, DEFAULT_RUNS_DIR);
-
         final String path = runsDir + "/" + runId;
-        final String exPlanUrl = path + "/execplan.json";
-        final String progressUrl = path + "/progress.json";
+        this.exPlanUrl = path + "/execplan.json";
+        this.progressUrl = path + "/progress.json";
+
         final FileSystem execplanFile = FileSystems.getFileSystem(exPlanUrl).get();
         try (final OutputStreamWriter writer = new OutputStreamWriter(execplanFile.create(exPlanUrl, true))) {
             HashMap<String, Object> jsonPlanMap = new HashMap<>();
@@ -41,16 +43,32 @@ public class Monitor {
             throw e.getCause();
         }
 
+        HashMap<String, Integer> initialProgress = new HashMap<>();
+        for (Map stage: initialExecutionPlan) {
+            for (Map operator: (List<Map>)stage.get("operators")) {
+                initialProgress.put((String)operator.get("name"), 0);
+            }
+        }
+        updateProgress(initialProgress);
+
+    }
+
+    public void updateProgress(HashMap<String, Integer> partialProgress) throws IOException {
+        HashMap<String, Object> progressBar = new HashMap<>();
+        Integer overall = 0;
+        for (String operatorName : partialProgress.keySet()) {
+            this.progress.put(operatorName, partialProgress.get(operatorName));
+        }
+
         final FileSystem progressFile = FileSystems.getFileSystem(progressUrl).get();
         try (final OutputStreamWriter writer = new OutputStreamWriter(progressFile.create(progressUrl, true))) {
-            HashMap<String, Object> progressMap = new HashMap<>();
-            progressMap.put("overall", "100");
-            JSONObject jsonPlan = new JSONObject(progressMap);
-            writer.write(jsonPlan.toString());
+            progressBar.put("overall", overall);
+            progressBar.put("details", progress);
+
+            JSONObject jsonProgress = new JSONObject(progressBar);
+            writer.write(jsonProgress.toString());
         } catch (UncheckedIOException e) {
             throw e.getCause();
         }
-
-
     }
 }
