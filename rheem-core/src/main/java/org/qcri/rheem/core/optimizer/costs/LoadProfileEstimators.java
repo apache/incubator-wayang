01bbf130a6de496974b90cae228ae8094404e056
Sebastian Kruse <sebastian.kruse@hpi.de>
2016-11-18 14:57:03 +0100

    feat(rheem-core): build in MathEx support for cost UDFs
    * MathEx is a light-weight library for mathematical expressions
    * add a dedicated, tuned MathEx context to retrieve variables

--- rheem-core/src/main/java/org/qcri/rheem/core/optimizer/costs/LoadProfileEstimators.java
+++ rheem-core/src/main/java/org/qcri/rheem/core/optimizer/costs/LoadProfileEstimators.java
@@ -1,10 +1,16 @@
 package org.qcri.rheem.core.optimizer.costs;
 
+import com.github.sekruse.mathex.Context;
+import com.github.sekruse.mathex.DefaultContext;
+import com.github.sekruse.mathex.Expression;
+import com.github.sekruse.mathex.ExpressionBuilder;
+import com.github.sekruse.mathex.exceptions.EvaluationException;
 import org.json.JSONObject;
 import org.qcri.rheem.core.api.Configuration;
 import org.qcri.rheem.core.api.exception.RheemException;
 import org.qcri.rheem.core.function.FunctionDescriptor;
 import org.qcri.rheem.core.optimizer.OptimizationContext;
+import org.qcri.rheem.core.optimizer.OptimizationUtils;
 import org.qcri.rheem.core.optimizer.cardinality.CardinalityEstimate;
 import org.qcri.rheem.core.optimizer.costs.LoadEstimator.SinglePointEstimationFunction;
 import org.qcri.rheem.core.plan.rheemplan.ExecutionOperator;
@@ -13,6 +19,7 @@ import org.qcri.rheem.core.util.ReflectionUtils;
 
 import java.util.*;
 import java.util.function.ToDoubleBiFunction;
+import java.util.function.ToDoubleFunction;
 import java.util.function.ToLongBiFunction;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -21,6 +28,20 @@ import java.util.stream.StreamSupport;
  * Utilities to deal with {@link LoadProfileEstimator}s.
  */
 public class LoadProfileEstimators {
+
+    /**
+     * Base {@link Context} that provides several functions for estimation purposes.
+     */
+    public static final Context baseContext;
+
+    static {
+        DefaultContext ctx = new DefaultContext(Context.baseContext);
+        ctx.setFunction(
+                "logGrowth",
+                vals -> OptimizationUtils.logisticGrowth(vals[0], vals[1], vals[2], vals[3])
+        );
+        baseContext = ctx;
+    }
 
     /**
      * Prevent instantiation of this class.
@@ -103,6 +124,84 @@ public class LoadProfileEstimators {
             );
         } catch (Exception e) {
             throw new RheemException(String.format("Could not initialize from specification \"%s\".", jsonJuelSpec), e);
+        }
+    }
+
+    /**
+     * Creates a new instance from a specification {@link String}. Valid specifications are as follows:
+     * <pre>
+     *     {"cpu":&lt;mathematical expression&gt;,
+     *      "ram":&lt;mathematical expression&gt;,
+     *      "disk":&lt;mathematical expression&gt;,
+     *      "network":&lt;mathematical expression&gt;,
+     *      "import":&lt;["optional", "operator", "properties"]&gt;,
+     *      "in":&lt;#inputs&gt;,
+     *      "out":&lt;#outputs&gt;,
+     *      "p":&lt;correctness probability&gt;,
+     *      "overhead":&lt;overhead in milliseconds&gt;,
+     *      "ru":&lt;resource utilization mathematical expression&gt;
+     *      }
+     * </pre>
+     * The JUEL expressions accept as parameters {@code in0}, {@code in1} a.s.o. for the input cardinalities and
+     * {@code out0}, {@code out1} a.s.o. for the output cardinalities.
+     *
+     * @param specification a specification that adheres to above format
+     * @return the new instance
+     */
+    public static NestableLoadProfileEstimator createFromSpecification(String specification) {
+        try {
+            final JSONObject spec = new JSONObject(specification);
+            int numInputs = spec.getInt("in");
+            int numOutputs = spec.getInt("out");
+            double correctnessProb = spec.getDouble("p");
+            List<String> operatorProperties = spec.has("import") ?
+                    StreamSupport.stream(spec.optJSONArray("import").spliterator(), false).map(Objects::toString).collect(Collectors.toList()) :
+                    Collections.emptyList();
+
+
+            LoadEstimator cpuEstimator = new DefaultLoadEstimator(
+                    numInputs,
+                    numOutputs,
+                    correctnessProb,
+                    CardinalityEstimate.EMPTY_ESTIMATE,
+                    compile(spec.getString("cpu"))
+            );
+            LoadEstimator ramEstimator = new DefaultLoadEstimator(
+                    numInputs,
+                    numOutputs,
+                    correctnessProb,
+                    CardinalityEstimate.EMPTY_ESTIMATE,
+                    compile(spec.getString("ram"))
+            );
+            LoadEstimator diskEstimator = !spec.has("disk") ? null : new DefaultLoadEstimator(
+                    numInputs,
+                    numOutputs,
+                    correctnessProb,
+                    CardinalityEstimate.EMPTY_ESTIMATE,
+                    compile(spec.getString("disk"))
+            );
+            LoadEstimator networkEstimator = !spec.has("network") ? null : new DefaultLoadEstimator(
+                    numInputs,
+                    numOutputs,
+                    correctnessProb,
+                    CardinalityEstimate.EMPTY_ESTIMATE,
+                    compile(spec.getString("network"))
+            );
+
+            long overhead = spec.has("overhead") ? spec.getLong("overhead") : 0L;
+            ToDoubleBiFunction<long[], long[]> resourceUtilizationEstimator = spec.has("ru") ?
+                    compileResourceUsage(spec.getString("ru")) :
+                    DEFAULT_RESOURCE_UTILIZATION_ESTIMATOR;
+            return new NestableLoadProfileEstimator(
+                    cpuEstimator,
+                    ramEstimator,
+                    diskEstimator,
+                    networkEstimator,
+                    resourceUtilizationEstimator,
+                    overhead
+            );
+        } catch (Exception e) {
+            throw new RheemException(String.format("Could not initialize from specification \"%s\".", specification), e);
         }
     }
 
@@ -190,6 +289,81 @@ public class LoadProfileEstimators {
     }
 
     /**
+     * Parses a mathematical expression and provides it as a {@link SinglePointEstimationFunction}.
+     *
+     * @param expression a mathematical expression
+     * @return the {@link SinglePointEstimationFunction}
+     */
+    private static SinglePointEstimationFunction compile(String expression) {
+        final Expression expr = ExpressionBuilder.parse(expression).specify(baseContext);
+        return (context, inCards, outCards) -> {
+            Context mathContext = createMathContext(context, inCards, outCards);
+            return Math.round(expr.evaluate(mathContext));
+        };
+    }
+
+    /**
+     * Parses a mathematical expression and provides it as a {@link ToDoubleFunction}.
+     *
+     * @param expression a mathematical expression
+     * @return a {@link ToLongBiFunction} wrapping the expression
+     */
+    private static ToDoubleBiFunction<long[], long[]> compileResourceUsage(String expression) {
+        final Expression expr = ExpressionBuilder.parse(expression).specify(baseContext);
+        return (inCards, outCards) -> {
+            Context mathContext = createMathContext(null, inCards, outCards);
+            return expr.evaluate(mathContext);
+        };
+    }
+
+
+    /**
+     * Create a mathematical {@link Context} from the parameters.
+     *
+     * @param context             provides miscellaneous variables
+     * @param inputCardinalities  provides input {@link CardinalityEstimate}s (`in***`)
+     * @param outputCardinalities provides output {@link CardinalityEstimate}s (`out***`)
+     * @return the {@link Context}
+     */
+    private static Context createMathContext(final EstimationContext context,
+                                             final long[] inputCardinalities,
+                                             final long[] outputCardinalities) {
+        return new Context() {
+            @Override
+            public double getVariable(String variableName) throws EvaluationException {
+                // Serve "in999" and "out999" variables directly from the cardinality arrays.
+                if (variableName.startsWith("in") && variableName.length() > 2) {
+                    int accu = 0;
+                    int i;
+                    for (i = 2; i < variableName.length(); i++) {
+                        char c = variableName.charAt(i);
+                        if (!Character.isDigit(c)) break;
+                        accu = 10 * accu + (c - '0');
+                    }
+                    if (i == variableName.length()) return inputCardinalities[accu];
+                } else if (variableName.startsWith("out") && variableName.length() > 3) {
+                    int accu = 0;
+                    int i;
+                    for (i = 3; i < variableName.length(); i++) {
+                        char c = variableName.charAt(i);
+                        if (!Character.isDigit(c)) break;
+                        accu = 10 * accu + (c - '0');
+                    }
+                    if (i == variableName.length()) return outputCardinalities[accu];
+                }
+
+                // Otherwise, ask the context for the property.
+                return context.getDoubleProperty(variableName, Double.NaN);
+            }
+
+            @Override
+            public ToDoubleFunction<double[]> getFunction(String functionName) throws EvaluationException {
+                throw new EvaluationException("This context does not provide any functions.");
+            }
+        };
+    }
+
+    /**
      * Estimate the {@link LoadProfile} for an {@link OptimizationContext.OperatorContext} using a
      * {@link LoadProfileEstimator} for the corresponding {@link ExecutionOperator}.
      *
@@ -210,7 +384,7 @@ public class LoadProfileEstimators {
     /**
      * Utility to nest the {@link LoadProfileEstimator} of a {@link FunctionDescriptor}.
      *
-     * @param mainEstimator      an optional {@link LoadProfileEstimator}; should be a {@link NestableLoadProfileEstimator}
+     * @param mainEstimatorOpt   an optional {@link LoadProfileEstimator}; should be a {@link NestableLoadProfileEstimator}
      * @param functionDescriptor whose {@link LoadProfileEstimator} should be nested
      * @param configuration      provides the UDF {@link LoadProfileEstimator}
      */
--- rheem-core/src/test/java/org/qcri/rheem/core/optimizer/costs/NestableLoadProfileEstimatorTest.java
+++ rheem-core/src/test/java/org/qcri/rheem/core/optimizer/costs/NestableLoadProfileEstimatorTest.java
@@ -20,7 +20,7 @@ import java.util.List;
 public class NestableLoadProfileEstimatorTest {
 
     @Test
-    public void testFromSpecification() {
+    public void testFromJuelSpecification() {
         String specification = "{" +
                 "\"in\":2," +
                 "\"out\":1," +
@@ -32,6 +32,37 @@ public class NestableLoadProfileEstimatorTest {
                 "}";
         final NestableLoadProfileEstimator estimator =
                 LoadProfileEstimators.createFromJuelSpecification(specification);
+        final LoadProfile estimate = estimator.estimate(new DummyEstimationContext(
+                new CardinalityEstimate[]{
+                        new CardinalityEstimate(10, 10, 1d), new CardinalityEstimate(100, 100, 1d)
+                },
+                new CardinalityEstimate[]{new CardinalityEstimate(200, 300, 1d)},
+                1
+        ));
+
+        Assert.assertEquals(3 * 10 + 2 * 100 + 7 * 200, estimate.getCpuUsage().getLowerEstimate(), 0.01);
+        Assert.assertEquals(3 * 10 + 2 * 100 + 7 * 300, estimate.getCpuUsage().getUpperEstimate(), 0.01);
+        Assert.assertEquals(
+                OptimizationUtils.logisticGrowth(0.1, 0.1, 10000, 100 + 10),
+                estimate.getResourceUtilization(),
+                0.000000001
+        );
+        Assert.assertEquals(143, estimate.getOverheadMillis());
+    }
+
+    @Test
+    public void testFromMathExSpecification() {
+        String specification = "{" +
+                "\"in\":2," +
+                "\"out\":1," +
+                "\"p\":0.8," +
+                "\"cpu\":\"3*in0 + 2*in1 + 7*out0\"," +
+                "\"ram\":\"6*in0 + 4*in1 + 14*out0\"," +
+                "\"overhead\":143," +
+                "\"ru\":\"logGrowth(0.1, 0.1, 10000, in0+in1)\"" +
+                "}";
+        final NestableLoadProfileEstimator estimator =
+                LoadProfileEstimators.createFromSpecification(specification);
         final LoadProfile estimate = estimator.estimate(new DummyEstimationContext(
                 new CardinalityEstimate[]{
                         new CardinalityEstimate(10, 10, 1d), new CardinalityEstimate(100, 100, 1d)
