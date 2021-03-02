bd2cea229446ee2072791b0103c8934cd77c626a
rodrigopardomeza <ro.pardo.meza@gmail.com>
2021-03-02 17:05:40 -0300

    [WAYANG-8][API-PYTHON] Protobuf command

--- wayang-api/wayang-api-rest/src/main/java/org/apache/wayang/api/rest/server/spring/decoder/pyplanreader.sh
+++ wayang-api/wayang-api-rest/src/main/java/org/apache/wayang/api/rest/server/spring/decoder/pyplanreader.sh
@@ -1,3 +1,3 @@
 protoc -I=/Users/rodrigopardomeza/wayang/incubator-wayang/protobuf \
---java_out=/Users/rodrigopardomeza/wayang/incubator-wayang/wayang-api/wayang-api-rest/src/main/java/org/apache/wayang/api/rest/server/spring/decoder \
+--java_out=/Users/rodrigopardomeza/wayang/incubator-wayang/wayang-api/wayang-api-rest/src/main/java/org/apache/wayang/api/rest/server/spring/general \
 /Users/rodrigopardomeza/wayang/incubator-wayang/protobuf/pywayangplan.proto
