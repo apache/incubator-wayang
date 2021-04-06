0efab7baa550b3d8928f335fb45deb9211a342b1
Bertty Contreras-Rojas <bertty@scalytics.io>
2021-04-06 11:21:27 -0400

    [WAYANG-28] Add Shipper components templates

--- wayang-plugins/wayang-hackit/wayang-hackit-core/src/main/java/org/apache/wayang/plugin/hackit/core/sniffer/shipper/Shipper.java
+++ wayang-plugins/wayang-hackit/wayang-hackit-core/src/main/java/org/apache/wayang/plugin/hackit/core/sniffer/shipper/Shipper.java
@@ -17,5 +17,92 @@
  */
 package org.apache.wayang.plugin.hackit.core.sniffer.shipper;
 
-public interface Shipper {
+import org.apache.wayang.plugin.hackit.core.sniffer.shipper.receiver.Receiver;
+import org.apache.wayang.plugin.hackit.core.sniffer.shipper.sender.Sender;
+
+import java.io.Serializable;
+import java.util.Iterator;
+
+public abstract class Shipper<T, ST, SenderObj extends Sender<ST>, ReceiverObj extends Receiver<T>> implements Iterator<T>, Serializable {
+
+    protected Sender sender_instance;
+    protected Receiver receiver_instance;
+
+    protected abstract Sender createSenderInstance();
+    protected abstract Receiver createReceiverInstance();
+
+    /** Connect with the a Message queue service*/
+    public void publish(ST value){
+        if(this.sender_instance == null){
+            throw new RuntimeException("The Sender of the Shipper is not instanciated");
+        }
+        this.sender_instance.send(value);
+    }
+
+    /** To subscribe as a producer */
+    public void subscribeAsProducer(){
+        this.sender_instance = this.createSenderInstance();
+        this.sender_instance.init();
+    }
+
+    public void subscribeAsProducer(String... topic){
+        this.subscribeAsProducer("default", topic);
+    }
+
+    public void subscribeAsProducer(String metatopic, String... topic){
+        this.subscribeAsProducer();
+        ((PSProtocol)this.sender_instance)
+                .addExchange(metatopic)
+                .addTopic(topic)
+        ;
+    }
+
+    /** Close connection */
+    public void unsubscribeAsProducer(){
+        if( this.sender_instance == null) return;
+        this.sender_instance.close();
+    }
+
+    /** To subscribe/unsubscribe as a consumer
+     * metatopic correspond to EXCHANGE_NAME
+     * topics correspond to bindingKeys
+     * */
+    public void subscribeAsConsumer(){
+        this.receiver_instance = this.createReceiverInstance();
+        this.receiver_instance.init();
+    }
+    public void subscribeAsConsumer(String... topic){
+        this.subscribeAsProducer("default", topic);
+    }
+    public void subscribeAsConsumer(String metatopic, String... topic){
+        this.subscribeAsConsumer();
+        ((PSProtocol)this.receiver_instance)
+                .addExchange(metatopic)
+                .addTopic(topic)
+        ;
+    }
+
+    /** Close connection */
+    public void unsubscribeAsConsumer() {
+        if( this.receiver_instance == null) return;
+        this.receiver_instance.close();
+    }
+
+    public void close(){
+        this.unsubscribeAsConsumer();
+        this.unsubscribeAsProducer();
+    }
+
+    @Override
+    public abstract boolean hasNext();
+
+    @Override
+    public abstract T next();
+
+    public Iterator<T> getNexts(){
+        if( this.receiver_instance == null){
+            throw new RuntimeException("The Receiver of the Shipper is not instanciated");
+        }
+        return this.receiver_instance.getElements();
+    }
 }
