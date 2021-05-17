793441744cab037ef01cbe0bcf287007b4319882
rodrigopardomeza <ro.pardo.meza@gmail.com>
2021-05-17 16:11:55 -0400

    [WAYANG-30] Executor Manager to follow created threads

---
+++ wayang-plugins/wayang-hackit/wayang-hackit-sidecar/src/main/java/org/apache/wayang/hackit/sidecar/webservice/ExecutorManager.java
@@ -0,0 +1,103 @@
+package org.apache.wayang.hackit.sidecar.webservice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorManager {
+
+    private static ExecutorService ex;
+    public static Map<UUID, Thread> threadsMap;
+    public static Map<UUID, List<String>> processesLogs;
+
+    public static void init(){
+        ex = Executors.newFixedThreadPool(1);
+        threadsMap = new HashMap<>();
+        processesLogs = new HashMap<>();
+    }
+
+    public ExecutorManager(){
+
+    }
+
+    public static UUID addThread(ProcessBuilder processNotExecuted) {
+
+        UUID key = UUID.randomUUID();
+        List<String> logs = new ArrayList<>();
+
+        processesLogs.put(key, logs);
+
+        ex.submit(() -> {
+            try {
+                Process p = processNotExecuted.start();
+                BufferedReader stdInput = new BufferedReader(new
+                        InputStreamReader(p.getInputStream()));
+                stdInput.lines()
+                        .forEach(
+                                line -> {
+                                    logs.add(line);
+                                }
+                        );
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return key;
+    }
+
+    public static UUID addThread(List<ProcessBuilder> processesNotExecuted) {
+
+        UUID key = UUID.randomUUID();
+        List<String> logs = new ArrayList<>();
+
+        processesLogs.put(key, logs);
+
+        ex.submit(() -> {
+            try {
+
+                BufferedReader stdInput;
+                for (ProcessBuilder pb : processesNotExecuted) {
+                    Process p = pb.start();
+                    stdInput = new BufferedReader(new
+                            InputStreamReader(p.getInputStream()));
+
+                    stdInput.lines()
+                            .forEach(
+                                    line -> {
+                                        logs.add(line);
+                                    }
+                            );
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+        return key;
+    }
+
+    public static List<String> getProcessLogs(UUID pID){
+
+        return ExecutorManager.processesLogs.get(pID);
+    }
+
+    public static List<String> getProcessLogs(UUID pID, Integer startFrom){
+
+        List<String> logs = ExecutorManager.processesLogs.get(pID);
+        List<String> ls = new ArrayList<>();
+
+        Object[] values = logs.toArray();
+        for (int i = startFrom - 1; i < values.length; i++) {
+            ls.add(values[i].toString());
+        }
+
+        return ls;
+    }
+
+}
