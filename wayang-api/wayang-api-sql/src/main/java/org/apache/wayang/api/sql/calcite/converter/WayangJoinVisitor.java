b6d846e9c93ae3ea52233981ae74059febe846f7
Kristian Moltke Reitzel <k.reitzel@outlook.dk>
2023-04-21 14:31:33 +0200

    Add visit

--- wayang-api/wayang-api-sql/src/main/java/org/apache/wayang/api/sql/calcite/converter/WayangJoinVisitor.java
+++ wayang-api/wayang-api-sql/src/main/java/org/apache/wayang/api/sql/calcite/converter/WayangJoinVisitor.java
@@ -18,7 +18,14 @@
 
 package org.apache.wayang.api.sql.calcite.converter;
 
+import org.apache.calcite.rel.core.Join;
+import org.apache.calcite.rex.RexCall;
+import org.apache.calcite.rex.RexInputRef;
+import org.apache.calcite.rex.RexNode;
 import org.apache.wayang.api.sql.calcite.rel.WayangJoin;
+import org.apache.wayang.basic.data.Record;
+import org.apache.wayang.basic.operators.JoinOperator;
+import org.apache.wayang.core.function.FunctionDescriptor;
 import org.apache.wayang.core.plan.wayangplan.Operator;
 
 public class WayangJoinVisitor extends WayangRelNodeVisitor<WayangJoin> {
@@ -29,9 +36,33 @@ public class WayangJoinVisitor extends WayangRelNodeVisitor<WayangJoin> {
 
     @Override
     Operator visit(WayangJoin wayangRelNode) {
+        Operator childOpLeft = wayangRelConverter.convert(wayangRelNode.getInput(0));
+        Operator childOpRight = wayangRelConverter.convert(wayangRelNode.getInput(1));
 
+        RexNode condition = ((Join) wayangRelNode).getCondition();
 
+        JoinOperator<Record, Record, Object> join = new JoinOperator(
+                new KeyExtractor0(condition), // pass the index of key for table1
+                new KeyExtractor1(condition), // pass the index of key for table2
+                Record.class,
+                Record.class,
+                Object.class);
 
-        return null;
+        childOpLeft.connectTo(0,join,0); //call connectTo on both operators (left and right)
+        childOpRight.connectTo(0,join,1);
+
+        return join;
     }
+
+    // TODO fix serializable function method
+    // extract the left key
+    private class KeyExtractor0 implements FunctionDescriptor.SerializableFunction<Record, Object> {
+
+        private final RexNode rexNode;
+        private final Integer index;
+        private KeyExtractor0(RexNode rexNode) {
+            this.rexNode = rexNode;
+            RexCall call = (RexCall) rexNode;
+            RexNode operand = call.getOperands().get(0);
+            RexInputRef rexInputRef = (RexInputRef) operand;
 }
