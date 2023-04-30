5a11e4b5901dcb35230257be0c0f11a63c571652
AdeelAslamUnimore <adeel.aslam@unimore.it>
2023-04-30 18:27:53 +0200

    Updates for monitoring the runtime metrics of underlying platform (Spark)

--- wayang-platforms/wayang-spark/code/main/java/org/apache/wayang/spark/monitoring/Intefaces/TaskMetric.java
+++ wayang-platforms/wayang-spark/code/main/java/org/apache/wayang/spark/monitoring/Intefaces/TaskMetric.java
@@ -1,4 +1,3 @@
-package org.apache.wayang.spark.monitoring.Intefaces;
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,6 +15,8 @@ package org.apache.wayang.spark.monitoring.Intefaces;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package org.apache.wayang.spark.monitoring.Intefaces;
 
 import java.io.Serializable;
 /**
