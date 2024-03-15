3f2d4a8cea08374f8a2b5dd1e0b90b30b73e1963
Juri Petersen <juri@hey.com>
2024-03-15 09:41:09 +0100

    Add drawflow and json routes

--- wayang-api/wayang-api-json/README.md
+++
@@ -1,7 +0,0 @@
-# Wayang API JSON
-
-## Execution
-
-```shell
-mvn package exec:java -Dexec.mainClass=org.apache.wayang.api.json.Main
-```
--- wayang-api/wayang-api-json/src/main/scala/Hello.scala
+++ wayang-api/wayang-api-json/src/main/scala/Hello.scala
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.wayang.api.json
 
 import zio._
@@ -7,15 +24,32 @@ import zio.Console._
 import scala.util.Try
 
 import org.apache.wayang.api.json.builder.JsonPlanBuilder
+import org.apache.wayang.api.json.operatorfromdrawflow.OperatorFromDrawflowConverter
+import org.apache.wayang.api.json.operatorfromjson.OperatorFromJson
+import org.apache.wayang.api.json.parserutil.ParseOperatorsFromDrawflow
 import org.apache.wayang.api.json.parserutil.ParseOperatorsFromJson
 import org.apache.wayang.api.json.operatorfromjson.OperatorFromJson
 
 object Hello extends ZIOAppDefault {
   val drawRoute =
-    Method.GET / "text" -> handler(Response.text("Hello World!"))
+    Method.POST / "wayang-api-json" / "submit-plan" / "drawflow-format" -> handler { (req: Request) =>
+     (for {
+        requestBody <- req.body.asString
+        operatorsFromDrawflow <- ZIO.fromTry(Try(ParseOperatorsFromDrawflow.parseOperatorsFromString(requestBody).get)) // Wrap with IO.fromTry to handle exceptions.
+        operatorsFromJson = operatorsFromDrawflow.flatMap(op => OperatorFromDrawflowConverter.toOperatorFromJson(op)) // This should be a pure function, so it's fine to keep it outside of IO.
+        result <- ZIO.attempt(new JsonPlanBuilder().setOperators(operatorsFromJson).execute())
+        responseBody <- ZIO.attempt {
+          if (operatorsFromJson.exists(op => op.cat == OperatorFromJson.Categories.Output))
+            "Success"
+          else
+            result.collect().toString()
+        }
+        resBody <- ZIO.succeed(Response.text(responseBody))
+     } yield resBody).orDie
+  }
 
   val jsonRoute =
-    Method.POST / "json" -> handler { (req: Request) =>
+    Method.POST / "wayang-api-json" / "submit-plan" / "json" -> handler { (req: Request) =>
      (for {
         requestBody <- req.body.asString
         operatorsFromJson <- ZIO.fromTry(Try(ParseOperatorsFromJson.parseOperatorsFromString(requestBody).get))
