3669bdebecb5235791d0c5ad9b6c42c9227f0cb6
halv <halv@itu.dk>
2024-11-06 16:35:04 +0100

    Added JavaGoogleCloudSourceSTorage and according mapping

--- wayang-platforms/wayang-java/src/main/java/org/apache/wayang/java/mapping/GoogleCloudStorageSourceMapping.java
+++ wayang-platforms/wayang-java/src/main/java/org/apache/wayang/java/mapping/GoogleCloudStorageSourceMapping.java
@@ -4,19 +4,19 @@ import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 
-import org.apache.wayang.basic.operators.AmazonS3Source;
+import org.apache.wayang.basic.operators.GoogleCloudStorageSource;
 import org.apache.wayang.core.mapping.Mapping;
 import org.apache.wayang.core.mapping.OperatorPattern;
 import org.apache.wayang.core.mapping.PlanTransformation;
 import org.apache.wayang.core.mapping.ReplacementSubplanFactory;
 import org.apache.wayang.core.mapping.SubplanPattern;
-import org.apache.wayang.java.operators.JavaAmazonS3Source;
+import org.apache.wayang.java.operators.JavaGoogleCloudStorageSource;
 import org.apache.wayang.java.platform.JavaPlatform;
 
 /**
- * Mapping from {@link AmazonS3Source} to {@link JavaAmazonS3Source}.
+ * Mapping from {@link GoogleCloudStorageSource} to {@link JavaGoogleClodStorageSource}.
  */
-public class AmazonS3SourceMapping implements Mapping {
+public class GoogleCloudStorageSourceMapping implements Mapping {
     
         @Override
     public Collection<PlanTransformation> getTransformations() {
@@ -30,15 +30,15 @@ public class AmazonS3SourceMapping implements Mapping {
     private SubplanPattern createSubplanPattern(){
         final OperatorPattern operatorPattern = new OperatorPattern(
                 "source", 
-                new org.apache.wayang.basic.operators.AmazonS3Source((String) null, (String) null, (String) null, (String) null),
+                new org.apache.wayang.basic.operators.GoogleCloudStorageSource((String) null, (String) null, (String) null, (String) null),
                 false
         );
         return SubplanPattern.createSingleton(operatorPattern);
     }
 
     private ReplacementSubplanFactory createReplacementSubplanFactory() {
-        return new ReplacementSubplanFactory.OfSingleOperators<AmazonS3Source>(
-                (matchedOperator, epoch) -> new JavaAmazonS3Source(matchedOperator).at(epoch)
+        return new ReplacementSubplanFactory.OfSingleOperators<GoogleCloudStorageSource>(
+                (matchedOperator, epoch) -> new JavaGoogleCloudStorageSource(matchedOperator).at(epoch)
         );
     }
 
---
+++ wayang-platforms/wayang-java/src/main/java/org/apache/wayang/java/operators/JavaGoogleCloudStorageSource.java
@@ -0,0 +1,91 @@
+package org.apache.wayang.java.operators;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.apache.wayang.basic.operators.AmazonS3Source;
+import org.apache.wayang.basic.operators.GoogleCloudStorageSource;
+import org.apache.wayang.core.api.exception.WayangException;
+import org.apache.wayang.core.optimizer.OptimizationContext;
+import org.apache.wayang.core.optimizer.OptimizationContext.OperatorContext;
+import org.apache.wayang.core.optimizer.costs.LoadProfileEstimators;
+import org.apache.wayang.core.platform.ChannelDescriptor;
+import org.apache.wayang.core.platform.ChannelInstance;
+import org.apache.wayang.core.platform.lineage.ExecutionLineageNode;
+import org.apache.wayang.core.util.Tuple;
+import org.apache.wayang.java.channels.StreamChannel;
+import org.apache.wayang.java.execution.JavaExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JavaGoogleCloudStorageSource extends GoogleCloudStorageSource implements JavaExecutionOperator {
+    private static final Logger logger = LoggerFactory.getLogger(JavaTextFileSource.class);
+
+    public JavaGoogleCloudStorageSource(String bucket, String blobName, String filePathToCredentialsFile, String inputUrl) throws IOException {
+        super(bucket, blobName, filePathToCredentialsFile, inputUrl);
+    }
+
+    public JavaGoogleCloudStorageSource(GoogleCloudStorageSource that) {
+        super(that);
+    }
+
+
+
+
+
+    @Override
+    public List<ChannelDescriptor> getSupportedInputChannels(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSupportedInputChannels'");
+    }
+
+    @Override
+    public List<ChannelDescriptor> getSupportedOutputChannels(int index) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSupportedOutputChannels'");
+    }
+
+    @Override
+    public Tuple<Collection<ExecutionLineageNode>, Collection<ChannelInstance>> evaluate(ChannelInstance[] inputs,
+            ChannelInstance[] outputs, JavaExecutor javaExecutor, OperatorContext operatorContext) {
+        assert inputs.length == this.getNumInputs();
+        assert outputs.length == this.getNumOutputs();
+
+        try {
+            
+            BufferedReader buffereadReder = new BufferedReader(new InputStreamReader(super.getInputStream()));
+            Stream<String> lines = buffereadReder.lines(); 
+            ((StreamChannel.Instance) outputs[0]).accept(lines);
+        }
+        catch (Exception e) {
+            throw new WayangException("Failed to read file from Google CLoud storage with error", e);
+        }
+
+
+    //TODO what to write here
+     ExecutionLineageNode prepareLineageNode = new ExecutionLineageNode(operatorContext);
+        prepareLineageNode.add(LoadProfileEstimators.createFromSpecification(
+                //TODO what to write here
+
+                "wayang.java.googlecloudstoragesource.load.prepare", javaExecutor.getConfiguration()
+        ));
+        ExecutionLineageNode mainLineageNode = new ExecutionLineageNode(operatorContext);
+        mainLineageNode.add(LoadProfileEstimators.createFromSpecification(
+                //TODO what to write here
+
+                "wayang.java.googlecloudstoragesource.load.main", javaExecutor.getConfiguration()
+        ));
+
+
+        outputs[0].getLineage().addPredecessor(mainLineageNode);
+
+        return prepareLineageNode.collectAndMark();
+    }
+    
+    
+}
