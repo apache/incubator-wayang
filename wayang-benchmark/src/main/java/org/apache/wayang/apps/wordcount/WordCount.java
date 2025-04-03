31f698134f0432d41e3b693db491cc61fe79516e
Christoffer Kristensen <73429946+ChristofferEmilKristensen@users.noreply.github.com>
2025-04-03 09:11:36 +0000

    align WordCount with as-is from main apache/incubator-wayang repository

--- wayang-benchmark/src/main/java/org/apache/wayang/apps/wordcount/WordCount.java
+++ wayang-benchmark/src/main/java/org/apache/wayang/apps/wordcount/WordCount.java
@@ -16,76 +16,65 @@
  * limitations under the License.
  */
 
-package org.apache.wayang.apps.wordcount;
+ package org.apache.wayang.apps.wordcount;
 
-import org.apache.wayang.api.JavaPlanBuilder;
-import org.apache.wayang.basic.data.Tuple2;
-import org.apache.wayang.core.api.Configuration;
-import org.apache.wayang.core.api.WayangContext;
-import org.apache.wayang.flink.Flink;
-import org.apache.wayang.java.Java;
-import org.apache.wayang.spark.Spark;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.OptionalDouble;
-
-public class WordCount {
-
-    public static void main(String[] args){
-
-        System.out.println("I AM RUNNING WORD COUNT CLASS IN MAIN FUNCTION!!!");
-
-        if (args.length == 0) {
-            System.err.print("Usage: <input file URL>");
-            System.exit(1);
-        }
-
-
-
-            if (args.length == 0) {
-                System.err.print("Usage: <platform1>[,<platform2>]* <input file URL>");
-                System.exit(1);
-            }
-
-        /* Get a plan builder */
-        WayangContext wayangContext = new WayangContext(new Configuration())
-                .withPlugin(Java.basicPlugin())
-                .withPlugin(Spark.basicPlugin());
-//                .withPlugin(Flink.basicPlugin());
-
-        JavaPlanBuilder planBuilder = new JavaPlanBuilder(wayangContext)
-                .withJobName("WordCount")
-                .withUdfJarOf(WordCount.class);
-
-        /* Start building the Apache WayangPlan */
-        Collection<Tuple2<String, Integer>> wordcounts = planBuilder
-                /* Read the text file */
-                .readGoogleCloudStorageFile(args[1], args[2], args[3]).withName("Load file")
-
-                /* Split each line by non-word characters */
-                .flatMap(line -> Arrays.asList(line.split("\\W+"))) 
-//                .withSelectivity(1, 100, 0.9)
-                .withName("Split words")
-
-                /* Filter empty tokens */
-                .filter(token -> !token.isEmpty())
-                .withName("Filter empty words")
-
-                /* Attach counter to each word */
-                .map(word -> new Tuple2<>(word.toLowerCase(), 1)).withName("To lower case, add counter")
-
-                // Sum up counters for every word.
-                .reduceByKey(
-                        Tuple2::getField0,
-                        (t1, t2) -> new Tuple2<>(t1.getField0(), t1.getField1() + t2.getField1())
-                )
-                .withName("Add counters")
-
-                /* Execute the plan and collect the results */
-                .collect();
-
-        System.out.println(wordcounts);
-    }
-}
-
+ import org.apache.wayang.api.JavaPlanBuilder;
+ import org.apache.wayang.basic.data.Tuple2;
+ import org.apache.wayang.core.api.Configuration;
+ import org.apache.wayang.core.api.WayangContext;
+ import org.apache.wayang.flink.Flink;
+ import org.apache.wayang.java.Java;
+ import org.apache.wayang.spark.Spark;
+ 
+ import java.util.Arrays;
+ import java.util.Collection;
+ 
+ public class WordCount {
+ 
+     public static void main(String[] args){
+ 
+         if (args.length == 0) {
+             System.err.print("Usage: <input file URL>");
+             System.exit(1);
+         }
+ 
+         /* Get a plan builder */
+         WayangContext wayangContext = new WayangContext(new Configuration())
+                 .withPlugin(Java.basicPlugin())
+                 .withPlugin(Spark.basicPlugin());
+ //                .withPlugin(Flink.basicPlugin());
+ 
+         JavaPlanBuilder planBuilder = new JavaPlanBuilder(wayangContext)
+                 .withJobName("WordCount")
+                 .withUdfJarOf(WordCount.class);
+ 
+         /* Start building the Apache WayangPlan */
+         Collection<Tuple2<String, Integer>> wordcounts = planBuilder
+                 /* Read the text file */
+                 .readTextFile(args[0]).withName("Load file")
+ 
+                 /* Split each line by non-word characters */
+                 .flatMap(line -> Arrays.asList(line.split("\\W+")))
+ //                .withSelectivity(1, 100, 0.9)
+                 .withName("Split words")
+ 
+                 /* Filter empty tokens */
+                 .filter(token -> !token.isEmpty())
+                 .withName("Filter empty words")
+ 
+                 /* Attach counter to each word */
+                 .map(word -> new Tuple2<>(word.toLowerCase(), 1)).withName("To lower case, add counter")
+ 
+                 // Sum up counters for every word.
+                 .reduceByKey(
+                         Tuple2::getField0,
+                         (t1, t2) -> new Tuple2<>(t1.getField0(), t1.getField1() + t2.getField1())
+                 )
+                 .withName("Add counters")
+ 
+                 /* Execute the plan and collect the results */
+                 .collect();
+ 
+         System.out.println(wordcounts);
+     }
+ }
