bb8ff5d5c9ae5e0f96228100b1f21fec2916d711
mspruc <madssejer@gmail.com>
2025-06-24 14:49:07 +0200

    Set stream onClose() to close the underlying BufferedReader freeing resources

--- wayang-platforms/wayang-java/src/main/java/org/apache/wayang/java/operators/JavaTextFileSource.java
+++ wayang-platforms/wayang-java/src/main/java/org/apache/wayang/java/operators/JavaTextFileSource.java
@@ -26,26 +26,22 @@ import org.apache.wayang.core.platform.ChannelDescriptor;
 import org.apache.wayang.core.platform.ChannelInstance;
 import org.apache.wayang.core.platform.lineage.ExecutionLineageNode;
 import org.apache.wayang.core.util.Tuple;
-import org.apache.wayang.core.util.fs.FileSystem;
-import org.apache.wayang.core.util.fs.FileSystems;
 import org.apache.wayang.java.channels.StreamChannel;
 import org.apache.wayang.java.execution.JavaExecutor;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.*;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
-import java.util.stream.Stream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,7 +53,7 @@ public class JavaTextFileSource extends TextFileSource implements JavaExecutionO
 
     private static final Logger logger = LoggerFactory.getLogger(JavaTextFileSource.class);
 
-    public JavaTextFileSource(String inputUrl) {
+    public JavaTextFileSource(final String inputUrl) {
         super(inputUrl);
     }
 
@@ -66,68 +62,97 @@ public class JavaTextFileSource extends TextFileSource implements JavaExecutionO
      *
      * @param that that should be copied
      */
-    public JavaTextFileSource(TextFileSource that) {
+    public JavaTextFileSource(final TextFileSource that) {
         super(that);
     }
 
     @Override
     public Tuple<Collection<ExecutionLineageNode>, Collection<ChannelInstance>> evaluate(
-            ChannelInstance[] inputs,
-            ChannelInstance[] outputs,
-            JavaExecutor javaExecutor,
-            OptimizationContext.OperatorContext operatorContext) {
+            final ChannelInstance[] inputs,
+            final ChannelInstance[] outputs,
+            final JavaExecutor javaExecutor,
+            final OptimizationContext.OperatorContext operatorContext) {
 
         assert inputs.length == this.getNumInputs();
         assert outputs.length == this.getNumOutputs();
 
-
-        String urlStr = this.getInputUrl().trim();
-        URL sourceUrl = null;
+        final String urlStr = this.getInputUrl().trim();
+        final URL sourceUrl;
 
         try {
             sourceUrl = new URL(urlStr);
-            String protocol = sourceUrl.getProtocol();
-            if ( protocol.startsWith("https") || protocol.startsWith("http")  ) {
-                HttpURLConnection connection2 = (HttpURLConnection) sourceUrl.openConnection();
-                connection2.setRequestMethod("GET");
-
-                // Check if the response code indicates success (HTTP status code 200)
-                if (connection2.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                    logger.info(">>> Ready to stream the data from URL: " + urlStr);
-                    // Read the data line by line and process it in the StreamChannel
-                    Stream<String> lines2 = new BufferedReader(new InputStreamReader(connection2.getInputStream())).lines();
-                    ((StreamChannel.Instance) outputs[0]).accept(lines2);
-                }
-            }
-            else {
-                FileSystem fs = FileSystems.getFileSystem(urlStr).orElseThrow(
-                        () -> new WayangException(String.format("Cannot access file system of %s.", urlStr))
-                );
-
-                final InputStream inputStream = fs.open(urlStr);
-                Stream<String> lines = new BufferedReader(new InputStreamReader(inputStream)).lines();
-                ((StreamChannel.Instance) outputs[0]).accept(lines);
-            }
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        } catch (ProtocolException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-                throw new WayangException(String.format("Reading %s failed.", urlStr), e);
+        } catch (final Exception e) {
+            throw new WayangException("Could not create URL from string: " + urlStr, e);
         }
 
-        ExecutionLineageNode prepareLineageNode = new ExecutionLineageNode(operatorContext);
+        final String protocol = sourceUrl.getProtocol();
+
+        final Stream<String> lines = (protocol.startsWith("https") || protocol.startsWith("http"))
+                ? this.streamFromURL(sourceUrl)
+                : this.streamFromFs(urlStr);
+
+        ((StreamChannel.Instance) outputs[0]).accept(lines);
+
+        final ExecutionLineageNode prepareLineageNode = new ExecutionLineageNode(operatorContext);
         prepareLineageNode.add(LoadProfileEstimators.createFromSpecification(
-                "wayang.java.textfilesource.load.prepare", javaExecutor.getConfiguration()
-        ));
-        ExecutionLineageNode mainLineageNode = new ExecutionLineageNode(operatorContext);
+                "wayang.java.textfilesource.load.prepare", javaExecutor.getConfiguration()));
+
+        final ExecutionLineageNode mainLineageNode = new ExecutionLineageNode(operatorContext);
         mainLineageNode.add(LoadProfileEstimators.createFromSpecification(
-                "wayang.java.textfilesource.load.main", javaExecutor.getConfiguration()
-        ));
+                "wayang.java.textfilesource.load.main", javaExecutor.getConfiguration()));
 
         outputs[0].getLineage().addPredecessor(mainLineageNode);
 
         return prepareLineageNode.collectAndMark();
+    }
+
+    /**
+     * @return Stream<String> from the provided URL
+     */
+    private Stream<String> streamFromURL(final URL sourceUrl) {
+        try {
+            final HttpURLConnection connection = (HttpURLConnection) sourceUrl.openConnection();
+            connection.setRequestMethod("GET");
+
+            // Check if the response code indicates success (HTTP status code 200)
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                logger.info(">>> Ready to stream the data from URL: " + sourceUrl.toString());
+                // Read the data line by line and process it in the StreamChannel
+                final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                return reader.lines().onClose(() -> {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            } else {
+                throw new WayangException("Connection with Http failed");
+            }
+        } catch (final Exception e) {
+            throw new WayangException(e);
+        }
+    }
+
+    /**
+     * @return Stream<String> from the file system
+     */
+    private Stream<String> streamFromFs(final String path) {
+        final File file = new File(URI.create(path));
+
+        try {
+            final BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            return reader.lines().onClose(() -> {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        } catch (final Exception e) {
+            throw new WayangException(e);
+        }
     }
 
     @Override
@@ -141,12 +166,12 @@ public class JavaTextFileSource extends TextFileSource implements JavaExecutionO
     }
 
     @Override
-    public List<ChannelDescriptor> getSupportedInputChannels(int index) {
+    public List<ChannelDescriptor> getSupportedInputChannels(final int index) {
         throw new UnsupportedOperationException(String.format("%s does not have input channels.", this));
     }
 
     @Override
-    public List<ChannelDescriptor> getSupportedOutputChannels(int index) {
+    public List<ChannelDescriptor> getSupportedOutputChannels(final int index) {
         assert index <= this.getNumOutputs() || (index == 0 && this.getNumOutputs() == 0);
         return Collections.singletonList(StreamChannel.DESCRIPTOR);
     }
