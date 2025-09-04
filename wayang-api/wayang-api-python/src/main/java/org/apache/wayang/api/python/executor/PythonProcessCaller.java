bad0cd2f20bc80d8616c0d4c07d50cf4be81851f
mspruc <madssejer@gmail.com>
2025-09-04 13:23:48 +0200

    tests and general code cleanup for python api

--- wayang-api/wayang-api-python/src/main/java/org/apache/wayang/api/python/executor/PythonProcessCaller.java
+++ wayang-api/wayang-api-python/src/main/java/org/apache/wayang/api/python/executor/PythonProcessCaller.java
@@ -18,51 +18,47 @@
 
 package org.apache.wayang.api.python.executor;
 
-import com.google.protobuf.ByteString;
-import org.apache.wayang.core.api.Configuration;
-import org.apache.wayang.core.api.exception.WayangException;
-import org.apache.wayang.core.util.ReflectionUtils;
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
-import java.util.Base64;
-import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.wayang.core.api.Configuration;
+import org.apache.wayang.core.api.exception.WayangException;
+import org.apache.wayang.core.util.ReflectionUtils;
 
 public class PythonProcessCaller {
 
-    private Process process;
-    private Socket socket;
-    private ServerSocket serverSocket;
+    private final Process process;
+    private final Socket socket;
+    private final ServerSocket serverSocket;
     private boolean ready;
 
-    //TODO How to get the config
-    private Configuration configuration;
+    // TODO How to get the config
+    private final Configuration configuration;
 
-    public PythonProcessCaller(ByteString serializedUDF){
+    public PythonProcessCaller() {
 
-        //TODO create documentation to how to the configuration in the code
+        // TODO create documentation to how to the configuration in the code
         this.configuration = new Configuration();
         this.configuration.load(ReflectionUtils.loadResource("wayang-api-python-defaults.properties"));
         this.ready = false;
-        byte[] addr = new byte[4];
-        addr[0] = 127; addr[1] = 0; addr[2] = 0; addr[3] = 1;
+        final byte[] addr = new byte[4];
+        addr[0] = 127;
+        addr[1] = 0;
+        addr[2] = 0;
+        addr[3] = 1;
 
         try {
-            /*TODO should NOT be assigned an specific port, set port as 0 (zero)*/
+            /* TODO should NOT be assigned an specific port, set port as 0 (zero) */
             this.serverSocket = new ServerSocket(0, 1, InetAddress.getByAddress(addr));
-            ProcessBuilder pb = new ProcessBuilder(
+            final ProcessBuilder pb = new ProcessBuilder(
                     Arrays.asList(
                             this.configuration.getStringProperty("wayang.api.python.path"),
-                            this.configuration.getStringProperty("wayang.api.python.worker")
-                    )
-            );
-            Map<String, String> workerEnv = pb.environment();
+                            this.configuration.getStringProperty("wayang.api.python.worker")));
+            final Map<String, String> workerEnv = pb.environment();
             workerEnv.put("PYTHON_WORKER_FACTORY_PORT", String.valueOf(this.serverSocket.getLocalPort()));
             workerEnv.put("PYTHONPATH", this.configuration.getStringProperty("wayang.api.python.env.path"));
 
@@ -70,26 +66,25 @@ public class PythonProcessCaller {
             pb.redirectError(ProcessBuilder.Redirect.INHERIT);
             this.process = pb.start();
 
-
             // Redirect worker stdout and stderr
-            //IDK redirectStreamsToStderr(worker.getInputStream, worker.getErrorStream)
+            // IDK redirectStreamsToStderr(worker.getInputStream, worker.getErrorStream)
 
             // Wait for it to connect to our socket
             this.serverSocket.setSoTimeout(10000);
-            try {
-                this.socket = this.serverSocket.accept();
-                this.serverSocket.setSoTimeout(0);
 
-                if(socket.isConnected())
-                    this.ready = true;
-
-            } catch (Exception e) {
-                System.out.println(e);
-                throw new WayangException("Python worker failed to connect back.", e);
-            }
-        } catch (Exception e){
-            System.out.println(e);
+        } catch (final Exception e) {
             throw new WayangException("Python worker failed");
+        }
+
+        try {
+            this.socket = this.serverSocket.accept();
+            this.serverSocket.setSoTimeout(0);
+
+            if (socket.isConnected())
+                this.ready = true;
+
+        } catch (final Exception e) {
+            throw new WayangException("Python worker failed to connect back.", e);
         }
     }
 
@@ -101,16 +96,16 @@ public class PythonProcessCaller {
         return socket;
     }
 
-    public boolean isReady(){
+    public boolean isReady() {
         return ready;
     }
 
-    public void close(){
+    public void close() {
         try {
             this.process.destroy();
             this.socket.close();
             this.serverSocket.close();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
         }
     }
