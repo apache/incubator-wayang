f39f1208bed645de9b19f269385f71ea1e0c01c3
Jun Wang <jizhixiaojunbao@gmail.com>
2026-07-11 13:33:24 +0800

    Revert "Ensure JDBC SQL statements end with semicolon"
    This reverts commit b043afac7518ae70751d70988d0cba103d933a43.

--- wayang-platforms/wayang-jdbc-template/src/test/java/org/apache/wayang/jdbc/operators/JdbcGlobalReduceOperatorTest.java
+++ wayang-platforms/wayang-jdbc-template/src/test/java/org/apache/wayang/jdbc/operators/JdbcGlobalReduceOperatorTest.java
@@ -109,6 +109,6 @@ class JdbcGlobalReduceOperatorTest extends OperatorTestBase {
             assertTrue(count > 0);
         }
 
-        assertEquals("SELECT COUNT(*) FROM testA;", sqlQueryChannelInstance.getSqlQuery());
+        assertEquals("SELECT COUNT(*) FROM testA", sqlQueryChannelInstance.getSqlQuery());
     }
 }
